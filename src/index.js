def172007c51e5c683fa9e80ec07a8641367ab5a
Vaibhav <vaibhavallewar3@gmail.com>
2025-01-07 18:50:48 +0530

    some changes.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 
 var typed = new Typed(".typing", {
-    strings:["Web Developer.", "Web Designer.", "NodeJS Developer.", "ReactJS Developer."],
-    typeSpeed:100,
-    backSpeed:60,
-    loop:true
+    strings: ["Web Developer.", "Web Designer.", "Node.js Developer.", "React Developer."],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
 });
+
+// Total Experience
+const totalExp = document.getElementById('total_exp');
+const today = new Date();
+const month = today.getMonth() > 0 ? today.getMonth() + ' Month' : '';
+totalExp.innerHTML = `${today.getFullYear() - 2023} Years ${month} `;
